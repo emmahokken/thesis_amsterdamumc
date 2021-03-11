5fc8587b019f1cf8f74206fb8261791d96ffcae4
Emma Hokken <emmahokken@gmail.com>
2021-03-11 14:59:34 +0100

    integrate dilation with noise compute

--- defacing/code/compute_noise.py
+++ defacing/code/compute_noise.py
@@ -4,6 +4,8 @@ import numpy as np
 import os
 import sys 
 
+from dilation import dilate
+
 # declare relevant prefixes and directories 
 prefix = '../../../../../..'
 root_path = prefix + '/data/projects/ahead/raw_gdata/'
@@ -12,55 +14,76 @@ root_path = prefix + '/data/projects/ahead/raw_gdata/'
 directories = [d[1] for d in  os.walk(root_path)][0]
 
 for d in directories: 
+
+    with open(f'../data/{d}/noise_stats.txt', 'r') as f:
+        # skip over first line 
+        f.readline()
+
+        # read file, column wise
+        rows = [[x for x in line.split(',')] for line in f]
+        cols = np.array([np.array(col, dtype=float) for col in zip(*rows)])
+
+        mean_real, mean_imag, std_real, std_imag = [c for c in cols]
+
+    dilation = dilate(f'{root_path}{d}')
+
+    print(dilation.shape)
+
     # iterate over the four echo times
     for echo in range(1,5):
         file = f'{root_path}{d}/{d}_inv2_{echo}_gdataCorrected.nii.gz'
-
+       
         # load specific echo file 
         data_load = nib.load(file)
         data = data_load.get_fdata(dtype=np.complex64)
 
         # transform to kspace, normalize by scale of 1/sqrt(n)
-        kspace = np.fft.fft(data, norm='ortho')
-    
-        noise_per_coil = []
+        # data_ifft = np.fft.ifftshift(data)
+        # kspace = np.fft.fft2(data, norm='ortho')
+        # kspace_ifft = np.fft.fftshift(np.fft.fft2(data_ifft, norm='ortho'))
+        # kspace_n = np.fft.fftshift(np.fft.ifftn(data_ifft, norm='ortho'))
+
+        # kn = np.fft.fftshift(np.fft.fftn(data, axes=(0,1,2), norm='ortho'))
+
+        # plt.imshow(kn[:,:,100,0].real, cmap='gray')
+        # plt.title('fftn')
+
+        # plt.show()
+
+        # exit()
 
         # iterate over coils 
-        for coil in range(kspace.shape[3]):
+        for coil in range(data.shape[3]):
 
-            coil_data = kspace[:,:,:,coil]
+            coil_data = data[:,:,:,coil]
+            print(coil_data.shape)
 
-            cube_front = 15
-            cube_back = 16
+            # deface coil image 
+            dilated_data = coil_data*dilation
 
-            # select 15*15*15 cube from the 8 corners of k-space
-            cube1 = coil_data[0:cube_front,0:cube_front,0:cube_front]       # 000
-            cube2 = coil_data[0:cube_front,0:cube_front,-cube_back:-1]      # 001
-            cube3 = coil_data[0:cube_front,-cube_back:-1:,-cube_back:-1]    # 011
-            cube4 = coil_data[-cube_back:-1:,-cube_back:-1,-cube_back:-1]   # 111
-            cube6 = coil_data[-cube_back:-1,-cube_back:-1,0:cube_front]     # 110
-            cube5 = coil_data[-cube_back:-1,0:cube_front,0:cube_front]      # 100
-            cube7 = coil_data[0:cube_front,-cube_back:-1,0:cube_front]      # 010
-            cube8 = coil_data[-cube_back:-1,0:cube_front,-cube_back:-1]     # 101
-
-            # concatinate cubes 
-            cubes = np.concatenate((cube1, cube2, cube3, cube4, cube5, cube6, cube7, cube8))
-            
-            # calculate mean and std 
-            mean_real = np.mean(cubes.real)
-            std_real = np.std(cubes.real)
-
-            mean_imag = np.mean(cubes.imag)
-            std_imag = np.std(cubes.imag)
-
+            print(dilation.shape)
             # generate new Gaussian noise from that distribution 
-            noise_real = np.random.normal(mean_real, std_real, coil_data.shape)
-            noise_imag = np.random.normal(mean_imag, std_imag, coil_data.shape)
+            noise_real = np.random.normal(mean_real[coil], std_real[coil], coil_data.shape)
+            noise_imag = np.random.normal(mean_imag[coil], std_imag[coil], coil_data.shape)
 
-            noise_per_coil.append([noise_real, noise_imag])
+            # convert back to image space 
+            imspace_noise_real = np.fft.ifft(noise_real, norm='ortho')
+            imspace_noise_imag = np.fft.ifft(noise_imag, norm='ortho')
+
+            noise_per_coil.append([imspace_noise_real, imspace_noise_imag])
+
         # somehow combine 32 coils 
         # add noise to defaced data 
 
 
 
-        exit()
+            exit()
+    # save new scan 
+    if not os.path.exists(f'{save_path}/{d}'):
+        os.makedirs(f'{save_path}/{d}')
+    save_dir = f'{save_path}/{d}/dilated_{scan_type}.nii'
+
+    nifit_image = nib.Nifti1Image(dataobj=filled_in, header=inv2_nii.header, affine=inv2_nii.affine)
+    nib.save(img=nifit_image, filename=save_dir)
+
+
--- defacing/code/dilation.py
+++ defacing/code/dilation.py
@@ -11,59 +11,33 @@ from tqdm import tqdm
 from defacing import deface
 
 
-# decalre root path and scan type
-root_path = '../../../../../data/projects/ahead/raw_gdata'
-scan_type = 'inv2_te1_m_corr'
-brain_mask_type = 'mask_inv2_te2_m_corr'
-save_path = '../results/dilation'
+def dilate(file_path, kw=5, kh=5, iters=13, brain_mask_type='mask_inv2_te2_m_corr'):
 
-# dilation parameters 
-kernel = np.ones((5,5), dtype=np.uint8)
-iters = 13
+    # dilation parameters 
+    kernel = np.ones((kw,kh), dtype=np.uint8)
+  
+    # get brain mask and scan from within directory
+    brain_mask_path = f'{file_path}/nii/{brain_mask_type}.nii'
 
-# iterate over all subjects
-for subdir, dirs, files in os.walk(root_path):
-    for d in tqdm(dirs):
-        # get brain mask and scan from within directory
-        brain_mask_path = f'{root_path}/{d}/nii/{brain_mask_type}.nii'
-        path = f'{root_path}/{d}/nii/{scan_type}.nii'
+    # load in brain mask (in two steps for saving later)
+    brain_mask_nii = nib.load(brain_mask_path)
+    brain_mask = brain_mask_nii.get_fdata()
 
-        # load in brain mask (in two steps for saving later)
-        # try:
-        brain_mask_nii = nib.load(brain_mask_path)
-        brain_mask = brain_mask_nii.get_fdata()
-        inv2_nii = nib.load(path)
-        inv2 = inv2_nii.get_fdata()
+    # dilate brain mask
+    dilation = cv2.dilate(brain_mask, kernel, iterations=iters)
+
+    # smooth edges
+    gaus_dilation = cv2.GaussianBlur(dilation, (kw,kh), 0)
+
+
+    return gaus_dilation
     
-        # small = inv2[:40,:40,64]
-        # print('mean', np.mean(small), 'std:', np.std(small))
-        # # plt.imshow(small)
-        # # plt.show()
-        # exit()
+    # create inverse mask of dilatio
+    inv_dilation = abs(dilation - 1)
+    inv_dilation[inv_dilation < 1] = 0
+    
+   
+    # fill in with (second inversion) scan and add noise to background 
+    filled_in = (inv2*gaus_dilation)
+    filled_in_noise = (inv2*gaus_dilation) + noise_mask
 
-        # dilate brain mask
-        dilation = cv2.dilate(brain_mask, kernel, iterations=iters)
-
-        # smooth edges
-        gaus_dilation = cv2.GaussianBlur(dilation, (5,5), 0)
-
-        # create inverse mask of dilation
-        inv_dilation = abs(dilation - 1)
-
-        # add random noise to background 
-        small = inv2[:40,:40,64]
-        noise = np.random.normal(np.mean(small), np.std(small),inv_dilation.shape)
-        noise_mask = noise * inv_dilation
-
-        # fill in with (second inversion) scan and add noise to background 
-        filled_in = (inv2*gaus_dilation) + noise_mask
-       
-        # save new scan 
-        if not os.path.exists(f'{save_path}/{d}'):
-            os.makedirs(f'{save_path}/{d}')
-        save_dir = f'{save_path}/{d}/dilated_{scan_type}.nii'
-
-        nifit_image = nib.Nifti1Image(dataobj=filled_in, header=inv2_nii.header, affine=inv2_nii.affine)
-        nib.save(img=nifit_image, filename=save_dir)
-
-    break 
