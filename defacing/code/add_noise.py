6333ebe6b997687e4067956306864b9818fd40e3
Emma Hokken <emmahokken@gmail.com>
2021-03-17 11:00:16 +0100

    bugfix that didn't use complex number

--- defacing/code/add_noise.py
+++ defacing/code/add_noise.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import nibabel as nib 
 import numpy as np 
 import os
+from scipy import ndimage
 import sys 
 from tqdm import tqdm 
 
@@ -14,8 +15,11 @@ save_path = '../results/dilation/'
 
 # gather all directories 
 directories = [d[1] for d in  os.walk(root_path)][0]
-
-for d in tqdm(directories[20:]): 
+subjects = ['0004', '0016', '0067', '0083']
+for d in tqdm(directories): 
+    print(d)
+    if not any(x in d for x in subjects):
+        continue
 
     with open(f'../data/{d}/noise_stats.txt', 'r') as f:
         # skip over first line 
@@ -32,8 +36,12 @@ for d in tqdm(directories[20:]):
     dilation = np.moveaxis(dilation, 2, 0)
     inv_dilation = np.moveaxis(inv_dilation, 2, 0)
 
+    dilation = np.fliplr(dilation)
+    inv_dilation = np.fliplr(inv_dilation)
+
     # iterate over the four echo times
     for echo in range(1,5):
+        print('Echo:', echo)
         file = f'{root_path}{d}/{d}_inv2_{echo}_gdataCorrected.nii.gz'
        
         # load specific echo file 
@@ -41,6 +49,8 @@ for d in tqdm(directories[20:]):
         data = data_load.get_fdata(dtype=np.complex64)
 
         defaced_image = []
+        no_noise = []
+
 
         # iterate over coils 
         for coil in range(data.shape[3]):
@@ -49,19 +59,22 @@ for d in tqdm(directories[20:]):
 
             # deface coil image 
             dilated_data = coil_data*dilation
-
+            no_noise.append(dilated_data)
+        
             # generate new Gaussian noise from that distribution and make space for brain in noise
-            noise_real = np.random.normal(mean_real[coil], std_real[coil], coil_data.shape) * inv_dilation
-            noise_imag = np.random.normal(mean_imag[coil], std_imag[coil], coil_data.shape) * inv_dilation
+            noise_real = np.random.normal(0, std_real[coil], coil_data.shape) * inv_dilation
+            noise_imag = np.random.normal(0, std_imag[coil], coil_data.shape) * inv_dilation
 
             # combine noise and coil image
-            final_coil_image_real = dilated_data.real + noise_real
-            final_coil_image_imag = dilated_data.imag + noise_imag
+            dilated_data.real += noise_real
+            dilated_data.imag += noise_imag
             
-            defaced_image.append(final_coil_image_real + final_coil_image_imag)
+            defaced_image.append(dilated_data)
+          
 
         defaced_image = np.moveaxis(np.array(defaced_image), 0, 3)
-       
+        no_noise = np.moveaxis(np.array(no_noise), 0, 3)
+
         # save new scan 
         if not os.path.exists(f'{save_path}{d}'):
             os.makedirs(f'{save_path}/{d}')
@@ -69,5 +82,12 @@ for d in tqdm(directories[20:]):
 
         nifit_image = nib.Nifti1Image(dataobj=defaced_image, header=data_load.header, affine=data_load.affine)
         nib.save(img=nifit_image, filename=save_dir)
+
+        save_dir = f'{save_path}/{d}/dilated_no_noise_{d}_inv2_{echo}_gdataCorrected.nii.gz'
+
+        nifit_image = nib.Nifti1Image(dataobj=no_noise, header=data_load.header, affine=data_load.affine)
+        nib.save(img=nifit_image, filename=save_dir)
+
+        break 
 
 
