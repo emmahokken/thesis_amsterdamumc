c4014b16954fbe20f0974cd687c8096a47fadb75
Emma Hokken <emmahokken@gmail.com>
2021-05-07 02:02:08 +0200

    new r1 contrast for figures

--- methods/sharpness_method.py
+++ methods/sharpness_method.py
@@ -8,14 +8,20 @@ from scipy import ndimage
 # load in r2* image 
 subj = 8
 r2_star_path = f'../sharpness/data/{subj:03}_r2star_gt.nii'
-r2_star_full_path = f'../../../../../data/projects/ahead/raw_gdata/Subcortex_{subj:04}_006_R02/nii/r2scorr.nii'
+r2_star_full_path = f'../../../../../data/projects/ahead/raw_gdata/Subcortex_{subj:04}_006_R02/nii/r1corr.nii'
 r2_star = nib.load(r2_star_path).get_fdata()
 r2_star_full = nib.load(r2_star_full_path).get_fdata()
-print(r2_star.shape)
 
 # plt.imshow(ndimage.rotate(r2_star_full[:,:,123],90))
 # plt.show()
-
-plt.imshow(ndimage.rotate(r2_star_full[80:170,100:170,121],90), cmap='gray')
-plt.axis('off')
+s = 124
+fig, ax = plt.subplots(figsize=(15,15))
+ax.imshow(ndimage.rotate(r2_star_full[80:170,100:170,s],90), cmap='gray')
+ax.axis('off')
 plt.savefig('ventricle.pdf')
+plt.show()
+
+plt.imshow(ndimage.rotate(r2_star_full[:,:,s],90), cmap='gray')
+plt.axis('off')
+plt.show()
+plt.savefig('whole_brain.pdf')
